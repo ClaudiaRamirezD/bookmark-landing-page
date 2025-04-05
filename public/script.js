d56f3bfeb56ac011f7acdaa016678e8940ba6214
Claudia Ramirez <clau8812@gmail.com>
2025-04-05 16:57:01 -0600

    error to be added

--- public/script.js
+++ public/script.js
@@ -1,20 +1,18 @@
-//Get Elements
-const openButton = document.querySelector('#menu-open');
-const closeButton = document.querySelector('#menu-close');
-const backdrop = document.querySelector('#backdrop');
+// ========== NAV MOBILE ==========
+const openButton = document.querySelector("#menu-open");
+const closeButton = document.querySelector("#menu-close");
+const backdrop = document.querySelector("#backdrop");
 const navmobile = document.querySelector(".header__nav-mobile");
 
-// Avoid scrolling
 function toggleScrollLock(lock) {
-    document.body.style.overflow = lock ? 'hidden' : '';
+    document.body.style.overflow = lock ? "hidden" : "";
 }
 
-
-openButton.addEventListener('click', () => {
-    backdrop.classList.remove('hidden');
+openButton.addEventListener("click", () => {
+    backdrop.classList.remove("hidden");
     backdrop.classList.add("flex");
-    navmobile.classList.add("hidden")
-    openBtn.setAttribute("aria-expanded", "true");
+    navmobile.classList.add("hidden");
+    openButton.setAttribute("aria-expanded", "true");
     toggleScrollLock(true);
 });
 
@@ -22,6 +20,38 @@ closeButton.addEventListener("click", () => {
     backdrop.classList.remove("flex");
     backdrop.classList.add("hidden");
     navmobile.classList.remove("hidden");
-    openBtn.setAttribute("aria-expanded", "false");
+    openButton.setAttribute("aria-expanded", "false");
     toggleScrollLock(false);
+});
+
+// ========== FAQ ACCORDION ==========
+document.addEventListener("DOMContentLoaded", () => {
+    const faqs = document.querySelectorAll(".faq");
+
+    faqs.forEach((faq) => {
+        faq.addEventListener("click", () => {
+        const isOpen = faq.getAttribute("aria-expanded") === "true";
+
+        // Cierra todos
+        faqs.forEach((item) => {
+            const answer = item.querySelector(".answer");
+            const icon = item.querySelector(".faq-icon");
+            item.setAttribute("aria-expanded", "false");
+            answer.classList.add("hidden");
+            answer.setAttribute("aria-hidden", "true");
+            icon.classList.remove("icon-active");
+        });
+
+        // Si estaba cerrado, lo abre
+        if (!isOpen) {
+            const answer = faq.querySelector(".answer");
+            const icon = faq.querySelector(".faq-icon");
+
+            faq.setAttribute("aria-expanded", "true");
+            answer.classList.remove("hidden");
+            answer.setAttribute("aria-hidden", "false");
+            icon.classList.add("icon-active");
+        }
+        });
+    });
 });
